97d9cbe19548146fd4ef997834cb1b990d478e16
Anatoly Pavlov <pavlovanatoly2012@gmail.com>
2016-02-13 13:44:45 -0800

    -LogReg last Updates of Feb.13, 2016
    I added data scaling before training the model. It improved its performance a bit.

--- scripts/bnp_logreg.py
+++ scripts/bnp_logreg.py
@@ -8,11 +8,10 @@ import pandas as pd
 import numpy as np
 
 from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import Imputer
 
-import matplotlib
-matplotlib.use("Agg") #Needed to save figures
+from sklearn.cross_validation import train_test_split
+
 import time
 import os
 import sklearn.metrics
@@ -23,10 +22,9 @@ from sklearn.linear_model import LogisticRegression
 ### Controlling Parameters
 output_col_name = "target"
 test_col_name = "PredictedProb"
-enable_feature_analysis = 1
 id_col_name = "ID"
-num_iterations = 5
 test_size = 0.05
+C = 1.0
 
 # load the datasets
 print("## Loading Data")
@@ -34,17 +32,17 @@ models_predictions_file = "../predictions/models_predictions.csv"
 train = pd.read_csv('../inputs/train.csv')
 test = pd.read_csv('../inputs/test.csv')
 
-# data processing and encoding
+# data processing
+print("## Data Processing")
 if os.path.isfile(models_predictions_file):
     models_predictions = pd.read_csv(models_predictions_file)
 else:
     models_predictions = pd.DataFrame()
  
 
-print("## Data Processing")
+# encoding data
+print("## Encoding Data")
 train = train.drop(id_col_name, axis=1)
-
-print("## Data Encoding")
 for f in train.columns:
     if train[f].dtype=='object':
         print(f)
@@ -53,7 +51,6 @@ for f in train.columns:
         train[f] = lbl.transform(list(train[f].values))
         test[f] = lbl.transform(list(test[f].values))
 
-# encoding training data set
 features = [s for s in train.columns.ravel().tolist() if s != output_col_name]
 print("Features: ", features)
 
@@ -62,11 +59,7 @@ imp.fit(train[features])
 train[features] = imp.transform(train[features])
 test[features] = imp.transform(test[features])
 
-# creating temp_test array to use to compute predictions with test data set
-temp_test = test
-temp_test = temp_test.drop(id_col_name, axis=1)
-
-# encoding test data set
+# splitting training data into training and cross validation sets
 X_pos = train[train[output_col_name] == 1]
 X_neg = train[train[output_col_name] == 0]
     
@@ -78,17 +71,31 @@ X_valid = pd.concat([X_valid_pos,X_valid_neg])
     
 X_train = X_train.iloc[np.random.permutation(len(X_train))]
 X_valid = X_valid.iloc[np.random.permutation(len(X_valid))]
-    
+   
 y_train = X_train[output_col_name]
 y_valid = X_valid[output_col_name]
+
+# creating temp_test array to use to compute predictions with test data set
+temp_test = test
+temp_test = temp_test.drop(id_col_name, axis=1)
 
 # deleting first column, which corresponds to output y, from both data sets
 X_train = X_train.drop(output_col_name, axis=1)
 X_valid = X_valid.drop(output_col_name, axis=1)
+#print(X_train)
+
+# scaling data
+print("## Scaling Data")
+X_train = preprocessing.scale(X_train)
+X_valid = preprocessing.scale(X_valid)
+temp_test = preprocessing.scale(temp_test)
+
+#print("## Mean of X_train = ", X_train.mean(axis=0))
+#print("## STD of X_train = ", X_train.std(axis=0))
 
 # fit a logistic regression model to the data
 print("## Training")
-model = LogisticRegression(C = 100.0)
+model = LogisticRegression(C = C)
 model.fit(X_train, y_train)
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
